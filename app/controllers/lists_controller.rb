50e4cff381e1aaa6c5f99c5379091d99a13c436e
luis miranda <luispsmiranda.96@gmail.com>
2022-05-19 16:40:48 +0100

    Ongoing in controllers

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,19 @@
 class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params[:id])
+    @list.save
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+puts "Cleaning up database..."
+Movie.destroy_all
+List.destroy_all
+Bookmark.destroy_all
+puts "Database cleaned"
+list_all = List.create(name: "favoritos")
+bookmark = Bookmark.new(comment: "excelent")
+bookmark.list = list_all
+url = "http://tmdb.lewagon.com/movie/top_rated"
+movies = JSON.parse(URI.open(url).read)['results']
+movies.each do |movie|
+  puts "Creating #{movie['title']}"
+  base_poster_url = "https://image.tmdb.org/t/p/original"
+  film = Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+    rating: movie['vote_average']
+  )
+  bookmark.movie = film
+end
+bookmark.save
