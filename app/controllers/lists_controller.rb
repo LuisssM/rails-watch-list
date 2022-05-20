1fad1377589704614dd30b89eed71257baf3c776
luis miranda <luispsmiranda.96@gmail.com>
2022-05-20 16:02:12 +0100

    now is the HTML part

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,11 +9,27 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(params[:id])
-    @list.save
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def show
     @list = List.find(params[:id])
   end
+end
+
+def destroy
+  @bookmark = Bookmark.find(params[:id])
+  @bookmark.destroy
+  redirect_to list_path(@bookmark.list_id)
+end
+
+private
+
+def list_params
+  params.require(:list).permit(:name)
 end
